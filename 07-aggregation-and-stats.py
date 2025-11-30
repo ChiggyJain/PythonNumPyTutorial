a91e8280fccf87f4ce79210b760279c06929940b
Chirag Jain <cjain9975@gmail.com>
2025-11-30 11:09:37 +0530

    Python Numpy Tutorial

---
+++ 07-aggregation-and-stats.py
@@ -0,0 +1,9 @@
+import numpy as np
+import time
+import sys
+
+print("\nargmax & argmin")
+arr = np.array([10, 30, 20, 40, 5])
+print(f"arr: {arr}")
+print(f"arr.argax(): {arr.argmax()}")
+print(f"arr.argmin(): {arr.argmin()}")
